a0126bbc736e2feb7e32469395e6f3be1ab96d09
goofwoof <heli0086@outlook.com>
2020-07-31 11:56:01 +0800

    e2

--- src/exercise_02/characterCounter.js
+++ src/exercise_02/characterCounter.js
@@ -5,6 +5,17 @@ export default function countCharacter(string, prediction) {
   //
   // * Please implement this function and pass all the tests in character_counter_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  //start
+  if(string == null || string == undefined){
+    return 0;
+  }
+  if(prediction == null || prediction == undefined || prediction.length == 0){
+    return string.length;
+  }
+  var count = 0;
+  var reg = new RegExp(prediction, 'g')
+  string.replace(reg,function (m, i) {count++;});
+  return count;
+  //end
+  //throw new Error('Please delete this line and implement the function');
 }
