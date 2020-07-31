ac0ad08e2d87f7b9f6f0447cf96161885c31f28e
goofwoof <heli0086@outlook.com>
2020-07-31 12:43:41 +0800

    e5

--- src/exercise_05/flattenArray.js
+++ src/exercise_05/flattenArray.js
@@ -5,6 +5,25 @@ export default function flattenArray(array) {
   //
   // * Please implement this function and pass all the tests in flatten_array_spec.js.
   // * Please do NOT modify the signature of the function.
+  if (array === undefined || array === null){
+    throw new Error('Flatten undefined or null array');
+  }
+  return flattenWithDepth(array, [], 1);//展开2leve
 
-  throw new Error('Please delete this line and implement the function');
+  //throw new Error('Please delete this line and implement the function');
+}
+
+function flattenWithDepth (array, result, depth) {
+  for (var i = 0; i < array.length; i++) {
+      var value = array[i]
+
+      // 判断是否到了指定的深度 或者 当前数组是否已经全部展开
+      if (depth > 0 && Array.isArray(value)) {
+          flattenWithDepth(value, result, depth - 1)
+      } else {
+          result.push(value)
+      }
+  }
+
+  return result
 }
