946c6eae888b9f88854255c1346d0f5c9c4f25f9
goofwoof <heli0086@outlook.com>
2020-07-31 12:08:07 +0800

    e3

--- src/exercise_03/rangeAndSum.js
+++ src/exercise_03/rangeAndSum.js
@@ -3,20 +3,39 @@ export function range(start, end) {
   // and `end`.
   //
   // Your target:
+
+  var length = Math.abs(end - start);
+  var array = [];
+  if (start <= end){
+    for(var i=0;i<length; i++){
+      array.push(start++);
+    }
+  }
+  else{
+    for(var i=0;i<length; i++){
+      array.push(start--);
+    }
+  }
+  return array;
   //
   // * Please implement this function and pass all the tests in range_and_array_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  //throw new Error('Please delete this line and implement the function');
 }
 
 export function sum(...numbers) {
   // This function sums all the numbers in the array and returns the final result.
   //
   // Your target:
+  var s = 0;
+  for (var i=numbers.length-1; i>=0; i--) {
+    s += numbers[i];
+  }
+  return s;
   //
   // * Please implement this function and pass all the tests in range_and_array_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  //throw new Error('Please delete this line and implement the function');
 }
